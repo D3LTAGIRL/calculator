2a204ff662397c4f0ba620462ef0fde40dc1670d
D3LTAGIRL <56973626+D3LTAGIRL@users.noreply.github.com>
2025-10-28 16:17:47 -0500

    Add functionality to clear button & number buttons

--- calculator.js
+++ calculator.js
@@ -30,10 +30,46 @@ function operate(num1, operator, num2) {
   }
 }
 
+function updateCalcDisplay() {
+  calcDisplay.setAttribute("value", currNum ? currNum : 0);
+}
+
+function handleButtonInput(event) {
+  switch (event.target.innerText) {
+    case "Clear":
+      prevNum = currNum = operator = null;
+      updateCalcDisplay();
+      break;
+
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      currNum = parseFloat(String(currNum ? currNum : 0) + event.target.innerText);
+      updateCalcDisplay();
+      break;
+    
+    default:
+      console.log(event);
+      break;
+      
+  }
+}
+
 const calcDisplay = document.querySelector("#calc-display");
+const calcButtons = document.querySelector("#calculator-buttons");
 
-calcDisplay.setAttribute("value", "0")
+let prevNum = null;
+let currNum = null;
+let operator = null;
 
-let prevNum;
-let currNum;
-let operator;
+calcButtons.addEventListener("click", handleButtonInput);
+
+updateCalcDisplay();
+
